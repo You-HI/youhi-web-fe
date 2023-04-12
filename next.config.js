150d0c2966b82753348a3a8464b18095f4c40df8
gabriel-yang <gabriel.yang@kakaocorp.com>
2023-04-13 00:17:29 +0900

    [feat] 이미지 경로 설정을 위한 next config 수정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const GITHUB_PAGE_URL = 'https://you-hi.github.io/youhi-web-fe';
 
 const nextConfig = {
   assetPrefix: process.env.NODE_ENV === 'production' ? GITHUB_PAGE_URL : '',
+  images: {
+    loader: 'imgix',
+    path: GITHUB_PAGE_URL,
+  }
 };
 
 module.exports = nextConfig;
