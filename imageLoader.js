12496e3086958997a55b322fd9a7c216e02679f0
yunhee_shin <qlyh8@naver.com>
2023-04-24 01:37:31 +0900

    fix: 리소스 경로 수정

--- imageLoader.js
+++
@@ -1,4 +0,0 @@
-export default function customImageLoader({ src, width, quality }) {
-  const url = new URL(`https://you-hi.github.io/youhi-web-fe${src}`);
-  return url.href;
-}
--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
-const GITHUB_PAGE_URL = 'https://you-hi.github.io/youhi-web-fe';
+const REPO = 'youhi-web-fe';
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  assetPrefix: isProd ? GITHUB_PAGE_URL : '',
-  images: isProd ? {
-    loader: 'default',
-    loaderFile: './imageLoader.js',
-  } : {
-    loader: 'default',
-    loaderFile: '',
-  }
+  assetPrefix: isProd ? `/${REPO}/` : '',
+  basePath: isProd ? `/${REPO}` : '/',
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
