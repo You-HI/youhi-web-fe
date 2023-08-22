a9089110850c467b6aef0fd8819308c93c0c2bee
yunhee_shin <qlyh8@naver.com>
2023-08-23 02:05:17 +0900

    feat: msw 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from 'next/app';
 import { ErrorBoundary } from '@/components/error';
 import '@/styles/globals.scss';
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'true') {
+  import('../src/mocks');
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
